ca8a3df37096e9f9f14b815ef9814f06cc6b3f47
masafumi terazono <hijirinhijirin@gmail.com>
2020-04-27 17:42:04 +0900

    ブロックをこわしたときのイベント、プレイヤーがプレイヤーをキルしたときのイベント処理を追加

--- src/main/java/jp/hack/minecraft/mineandfight/logic/MineAndFight.java
+++ src/main/java/jp/hack/minecraft/mineandfight/logic/MineAndFight.java
@@ -3,6 +3,8 @@ package jp.hack.minecraft.mineandfight.logic;
 import jp.hack.minecraft.mineandfight.core.Game;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.logging.LogManager;
@@ -20,6 +22,19 @@ public class MineAndFight implements Listener {
     @EventHandler
     public void onLogin(PlayerJoinEvent event) {
         logger.info(String.format("onLogin: %s", event.getPlayer().getName()));
+    }
+
+    @EventHandler
+    public void onBlockBreakEvent(BlockBreakEvent event){
+        logger.info(String.format("onBlockBreakEvent: %s", event.getPlayer().getName()));
+
+    }
+
+    @EventHandler
+    public void onPlayerDeathEvent(PlayerDeathEvent event) {
+        String killed = event.getEntity().getName();
+        String killer = event.getEntity().getKiller().getName();
+        logger.info(String.format("onPlayerDeathEvent: %s -> %s", killer, killed));
     }
 
 }
