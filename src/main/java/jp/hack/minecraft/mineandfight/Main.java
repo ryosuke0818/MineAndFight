a241d5701960b5628243ac2e20110081af199e51
masafumi terazono <hijirinhijirin@gmail.com>
2020-05-02 09:59:00 +0900

    コンパイル通らなくなっていたので修正しときました。（あくまでとおるようにしただけ）

--- src/main/java/jp/hack/minecraft/mineandfight/Main.java
+++ src/main/java/jp/hack/minecraft/mineandfight/Main.java
@@ -4,13 +4,8 @@ import jp.hack.minecraft.mineandfight.command.HostCommandExecutor;
 import jp.hack.minecraft.mineandfight.command.PlayerCommandExecutor;
 import jp.hack.minecraft.mineandfight.core.Game;
 import jp.hack.minecraft.mineandfight.logic.MineAndFight;
-<<<<<<< HEAD
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-=======
 import jp.hack.minecraft.mineandfight.utils.Configuration;
 import jp.hack.minecraft.mineandfight.utils.I18n;
->>>>>>> 0e925faa164ec2ab829eae566c1777e3dda4caa4
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
