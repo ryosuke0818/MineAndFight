42da0e3f145931de5ccce2de0345ccdacd7728cf
masafumi terazono <hijirinhijirin@gmail.com>
2020-05-05 12:23:23 +0900

    Ecnomyを追加

--- src/main/java/jp/hack/minecraft/mineandfight/core/GamePlugin.java
+++ src/main/java/jp/hack/minecraft/mineandfight/core/GamePlugin.java
@@ -2,13 +2,18 @@ package jp.hack.minecraft.mineandfight.core;
 
 import jp.hack.minecraft.mineandfight.core.utils.Configuration;
 import jp.hack.minecraft.mineandfight.core.utils.I18n;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 public abstract class GamePlugin extends JavaPlugin {
+    protected static final Logger LOGGER = Logger.getLogger("MineAndFightLogic");
     private transient Configuration configuration;
     private transient I18n i18n;
+    private static Economy econ = null;
 
     @Override
     public void onEnable() {
@@ -25,6 +30,12 @@ public abstract class GamePlugin extends JavaPlugin {
         this.configuration = new Configuration(new File(getDataFolder(), "config.yml"));
         this.configuration.setTemplateName("config.yml");
         this.configuration.load();
+
+        if (!setupEconomy() ) {
+            LOGGER.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
     }
 
     @Override
@@ -37,6 +48,22 @@ public abstract class GamePlugin extends JavaPlugin {
         if (this.i18n != null) {
             this.i18n.onDisable();
         }
+    }
+
+    public Economy getEconomy(){
+        return econ;
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
     }
 
     public Configuration getConfiguration() {
