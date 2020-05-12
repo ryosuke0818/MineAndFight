6cee79c3c0ce87d935fc01eef87da6aa98759e4d
masafumi terazono <hijirinhijirin@gmail.com>
2020-05-12 09:27:09 +0900

    ゲーム毎にイベントをフィルタリングする処理と、プレイヤーがゲームエリア外にでないようにする処理を追加

--- src/main/java/jp/hack/minecraft/mineandfight/core/GameManager.java
+++ src/main/java/jp/hack/minecraft/mineandfight/core/GameManager.java
@@ -1,17 +1,21 @@
 package jp.hack.minecraft.mineandfight.core;
 
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.Vector;
 
 public class GameManager implements Listener {
     private static class SingletonHolder {
@@ -40,17 +44,51 @@ public class GameManager implements Listener {
 
     @EventHandler
     public void onLogin(PlayerJoinEvent event) {
-        games.values().stream().forEach(g -> g.onLogin(event));
+        for(Iterator<Game> ite=games.values().iterator(); ite.hasNext();){
+            Game g = ite.next();
+            if(g.findPlayer(event.getPlayer().getUniqueId())!=null) {
+                g.onLogin(event);
+                break;
+            }
+        }
     }
 
     @EventHandler
+    public void onPlayerMoveEvent​(PlayerMoveEvent event​) {
+        //ゲームエリア外にでた場合は処理をキャンセルする
+        for(Iterator<Game> ite=games.values().iterator(); ite.hasNext();) {
+            Game g = ite.next();
+            if(g.findPlayer(event​.getPlayer().getUniqueId())!=null){
+                if(g.getGameArea().contains(event​.getTo().toVector()) != true){
+                    event​.setCancelled(true);
+                    break;
+                }
+            }
+        }
+    }
+
+
+    @EventHandler
     public void onBlockBreakEvent(BlockBreakEvent event) {
-        games.values().stream().forEach(g -> g.onBlockBreakEvent(event));
+        Location loc = event.getBlock().getLocation();
+        for(Iterator<Game> ite=games.values().iterator(); ite.hasNext();) {
+            Game g = ite.next();
+            if(g.getGameArea().contains(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ())) {
+                g.onBlockBreakEvent(event);
+                break;
+            }
+        }
     }
 
     @EventHandler
     public void onPlayerDeathEvent(PlayerDeathEvent event) {
-        games.values().stream().forEach(g -> g.onPlayerDeathEvent(event));
+        for(Iterator<Game> ite=games.values().iterator(); ite.hasNext();){
+            Game g = ite.next();
+            if(g.findPlayer(event.getEntity().getUniqueId())!=null) {
+                g.onPlayerDeathEvent(event);
+                break;
+            }
+        }
     }
 
 
